8f077b64047d2bd56831ef4e0cc5dedf34209bd5
cy3disc <73651004+cy3disc@users.noreply.github.com>
2021-03-17 15:45:27 +0100

    fdasfasd

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js")
 
 module.exports = bot => { 
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("b!help", {type: ""});
+    bot.user.setActivity("b!help   |   V.1.1", {type: ""});
 }
